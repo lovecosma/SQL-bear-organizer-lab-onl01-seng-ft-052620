005cfea796fcf0b39a32ba1590f98b8971f4cd52
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-24 17:13:33 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -12,7 +12,7 @@ end
 
 def selects_oldest_bear_and_returns_name_and_age
   "SELECT MAX(age) AS oldest_bear FROM bears;"
-  "SELECT name, age FROM bears"
+  "SELECT name, age FROM bears WHERE age = oldest_bear"
 end
 
 def select_youngest_bear_and_returns_name_and_age

b6be3e38bb5fcff2ef524d22a7626f303dd8bf82
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-24 18:08:28 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, "NULL", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, "NULL", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, "NULL", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, "NULL", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, "NULL", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, "NULL", NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, "NULL", NULL, NULL, NULL, NULL, NULL);
